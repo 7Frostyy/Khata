d4269b641f4664dd723e7f8ab5e5f68115888f30
7Frosty <64210720+7Frostyy@users.noreply.github.com>
2021-11-12 22:19:21 +0600

    v0.01

--- Khata.py
+++ Khata.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox, filedialog
@@ -9,15 +10,97 @@ def createWidgets():
     textArea = Text(root)
     textArea.grid(sticky = N+E+S+W)
 
+
+
     menuBar = Menu(root)
     root.config(menu=menuBar)
     fileMenu = Menu(menuBar, tearoff=0)
-    fileMenu.add_command(label="New", command="")
-    fileMenu.add_command(label="Open", command="")
-    fileMenu.add_command(label="Save", command="")
+    fileMenu.add_command(label="New", command=newFile)
+    fileMenu.add_command(label="Open", command=openFile)
+    fileMenu.add_command(label="Save", command=saveFile)
     fileMenu.add_separator()
-    fileMenu.add_command(label="Exit", command="")
+    fileMenu.add_command(label="Exit", command=exit)
     menuBar.add_cascade(label="File", menu=fileMenu)
+
+    editMenu = Menu(menuBar, tearoff=0)
+    editMenu.add_command(label="Cut", command=cut)
+    editMenu.add_command(label="Copy", command=copy)
+    editMenu.add_command(label="Paste", command=paste)
+    menuBar.add_cascade(label="Edit", menu=editMenu)
+
+    helpMenu = Menu(menuBar, tearoff=0)
+    helpMenu.add_command(label="About", command=help)
+    menuBar.add_cascade(label="Help", menu=helpMenu)
+
+
+def newFile():
+    global textArea
+    root.title("Untitled - Notepad")
+    file = None
+    textArea.delete(1.0, END)
+
+def openFile():
+    global textArea
+    file = filedialog.askopenfile(defaultextension=".txt", filetypes=[("All files", "*.*"), ("Text Documents", "*.txt")])
+    file = file.name
+
+    if file == "":
+        file = None
+    else:
+        import os
+        root.title(os.path.basename(file) + " - Notepad")
+        textArea.delete((1.0, END))
+        file = open(file, "rb")
+        textArea.insert(1.0, file.read())
+        file.close()
+
+    def newFile():
+        global textArea
+        root.title("Untitled - Notepad")
+        file = None
+        textArea.delete(1.0, END)
+
+def saveFile():
+    global textArea, file
+
+    if file == None:
+        file = filedialog.asksaveasfilename(initialfile="Untitled.txt", defaultextension=".txt", filetypes=[("All files", "*.*"), ("Text Documents", "*.txt")])
+
+        if file == None:
+            file = None
+        else:
+            file = open(file, "w")
+            file.write(textArea.get(1.0, END))
+            file.close()
+            file = file.name
+            root.title(os.path.basename(file) + " - NotePad")
+
+    else:
+        file = open(file, "w")
+        file.write(textArea.get(1.0, END))
+        file.close()
+
+
+
+def exit():
+    root.destroy()
+
+def cut():
+    global textArea
+    textArea.event_generate("<<Cut>>")
+
+def copy():
+    global textArea
+    textArea.event_generate("<<Copy>>")
+
+def paste():
+    global textArea
+    textArea.event_generate("<<Paste>>")
+
+def help():
+
+    messagebox.showinfo("NotePad", "This NotePad is Developed By Shikto")
+
 root = tk.Tk()
 root.title("Notepad")
 file = None
